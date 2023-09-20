2d7e4a9845c7ecb7bfe32043b2dc4e56a1b22681
tienmanh2405 <tienmanh0167@gmail.com>
2023-09-20 19:22:44 +0700

    add sua code phan ab

--- Lesson6_JavaScript/JS/phuongTrinhBac2.js
+++ Lesson6_JavaScript/JS/phuongTrinhBac2.js
@@ -21,16 +21,14 @@ if (a == 0)
     }
 } else {
     var delta = b * b - 4 * a * c;
-    if (denta > 0) {
+    if (delta > 0) {
         x1 = (-b + Math.sqrt(delta)) / (2 * a);
         x2 = (-b - Math.sqrt(delta)) / (2 * a);
         console.log("Nghiệm thứ nhất là: ", x1);
         console.log("Nghiệm thứ hai là: ", x2);
-    } else if (denta == 0) {
+    } else if (delta == 0) {
         console.log("Phương trình có nghiệm kép x1 = x2 =", -b / (2 * a));
     } else {
         console.log("Phương trình vô nghiệm");
     }
 }
-
-
